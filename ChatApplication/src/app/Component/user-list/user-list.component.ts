284a963f245f23b5058ab098e75440dd9e70ea4f
SahilGyanchandani <132891260+SahilGyanchandani@users.noreply.github.com>
2023-07-16 11:10:04 +0530

    Frontent Chat Application

--- ChatApplication/src/app/Component/user-list/user-list.component.ts
+++ ChatApplication/src/app/Component/user-list/user-list.component.ts
@@ -1,9 +1,8 @@
-import { Component, Output} from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { LoginServiceService } from 'src/app/Services/login-service.service';
 
-interface Msg{
-  content:string;
-  
+interface Message {
+  content: string;
 }
 
 @Component({
@@ -12,33 +11,26 @@ interface Msg{
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
- 
-  Users:any;
-  Msg:Msg[]=[];
+  Users: any;
+  Msg: Message[] = []; // Initialize Msg as an empty array
+
   selectedUserId: string = '';
 
-
-  constructor(private userService:LoginServiceService){ 
-      this.userService.onUserList().subscribe((data)=>{
-        // console.warn("Data",data);
-        this.Users=data;
-      })
-    }
-
-  getUserConversation(user:any): void {
-    this.userService.onMsgHistory(user.userID).subscribe((data: any[]
-      
-      )=>{
-      console.log(data);
-      this.Msg=data;
-      // console.log(this.Msg);
-      
-      
-      
-      
-    })
-    
-    
+  constructor(private userService: LoginServiceService) {
+    this.userService.onUserList().subscribe((data) => {
+      this.Users = data;
+    });
   }
 
+  getUserConversation(user: any): void {
+    this.userService.onMsgHistory(user.userID).subscribe((data: any) => {
+      console.log('Data from API:', data);
+      if (data && data.messages && Array.isArray(data.messages)) {
+        this.Msg = data.messages; // Access the messages array and assign it to Msg
+        console.log('Msg array:', this.Msg);
+      } else {
+        this.Msg = []; // If the data is not in the expected format, initialize as an empty array
+      }
+    });
+  }
 }
